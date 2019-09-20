b4a036506aa21ad1fcda65936477b0c7d1282eb7
SamYStudiO <contact@samystudio.net>
2019-09-21 00:02:57 +0200

    Updates

--- precache-manifest.e129bf5e28959af7c01be4e390fc17a8.js
+++ precache-manifest.e129bf5e28959af7c01be4e390fc17a8.js
@@ -1,18 +1,18 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "221f95cf10464ebcb9ab4a6aa107a4a8",
+    "revision": "b177ec7ff4ac0285a7018c0a6dabea57",
     "url": "/es-theme-next-pixel/index.html"
   },
   {
-    "revision": "172e04fbf07b8177982e",
-    "url": "/es-theme-next-pixel/static/css/main.b100e6da.chunk.css"
+    "revision": "a74959d648c73fafa7bd",
+    "url": "/es-theme-next-pixel/static/css/main.cd93d9ac.chunk.css"
   },
   {
     "revision": "44845c5340b12806b6b1",
     "url": "/es-theme-next-pixel/static/js/2.78512bdb.chunk.js"
   },
   {
-    "revision": "172e04fbf07b8177982e",
+    "revision": "a74959d648c73fafa7bd",
     "url": "/es-theme-next-pixel/static/js/main.ac8cf5c4.chunk.js"
   },
   {
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/es-theme-next-pixel/precache-manifest.9112840f727308fa329687e5e757b56e.js"
+  "/es-theme-next-pixel/precache-manifest.e129bf5e28959af7c01be4e390fc17a8.js"
 );
 
 self.addEventListener('message', (event) => {

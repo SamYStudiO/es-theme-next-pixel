5da6ac1e5a890a930452bf2597c793b388618ba8
SamYStudiO <contact@samystudio.net>
2021-06-30 14:55:15 -0400

    Add back fba as it seems to be used with some boards

---
+++ fba/games.sh
@@ -0,0 +1,19 @@
+list=(
+# Edit game list below (full game list available from ./data/games folder)
+donkey_kong
+galaga_arcade_fba_fbneo_mame
+operation_wolf
+pac_man
+space_invaders
+street_fighter
+time_crisis_ii
+# Do not edit below this line
+)
+
+games="$(IFS=,; printf '%s' "${list[*]}")"
+sed -i "s/random(.*)/random(${games})/" game_theme_color_accent.xml
+sed -i "s/random(.*)/random(${games})/" game_theme_gamelist.xml
+sed -i "s/random(.*)/random(${games})/" game_theme_horizontal.xml
+sed -i "s/random(.*)/random(${games})/" game_theme_horizontal_legacy.xml
+sed -i "s/random(.*)/random(${games})/" game_theme_music.xml
+sed -i "s/random(.*)/random(${games})/" game_theme_vertical_wheel.xml
